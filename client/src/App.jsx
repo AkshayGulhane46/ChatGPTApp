573357bd8498ba21a20b78ff9b4b2fa8dc9fa37b
Akshay Gulhane <argulhane10246@gmail.com>
2024-06-26 22:54:27 +0530

    Commit2606241

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import {BrowserRouter, Routes, Route , Navigate} from 'react-router-dom';
 
 function App() {
   
@@ -6,7 +7,12 @@ function App() {
   return (
     <>
         <div className='app'>
-          app
+          <BrowserRouter>
+              <Routes>
+                <Route path="/chat" element={<Chat/>}></Route>
+              </Routes>
+          </BrowserRouter>
+
         </div>
     </>
   )
