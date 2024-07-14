05c32c83858b20695a49cf412f7fe01ee3acf17c
Akshay Gulhane <argulhane10246@gmail.com>
2024-07-14 13:12:59 +0530

    Commit1407241

---
+++ client/src/components/customHeader/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {ChatBubbleLeftRightIcon , PhoneIcon} from "@heroicons/react/24/solid"
+
+const customHeader = () => {
+  return (
+    <div className=''></div>
+  )
+}
+
+export default customHeader
